a680a8bc7f108eefdc640f82c3ac91f050200788
Beverly Jean-Baptiste <tizha1990@gmail.com>
2020-09-09 10:44:09 -0500

    Styled course views and homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index]
   def index
+    @courses = Course.all.limit(3)
+    @recent_courses = Course.all.order(created_at: :desc).limit(3)
   end
 end
