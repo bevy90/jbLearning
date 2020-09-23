418912f2337d6baaa6464b7e5394dfa7c05b10c6
Beverly Jean-Baptiste <tizha1990@gmail.com>
2020-09-23 12:46:43 -0500

    Added gems rolify and pundit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable
+  
+  rolify
+  after_create :assign_default_role
 
   has_many :courses
+
+  validate :must_have_a_role, on: :update
   
   def to_s
     email
@@ -13,5 +18,16 @@ class User < ApplicationRecord
 
   def username
     self.email.split(/@/).first
+  end
+
+  def assign_default_role
+    self.add_role(:student) if self.roles.blank?
+  end
+
+  private
+  def must_have_a_role
+    unless roles.any?
+      errors.add(:roles, "must have at least one role")
+   end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit, :show, :update]
   get 'pages/index'
   get 'pages/activity'
   root 'pages#index'
