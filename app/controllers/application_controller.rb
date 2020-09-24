0fcb054138e7dc9079f5c73994029eee2f8b7bd7
Beverly Jean-Baptiste <tizha1990@gmail.com>
2020-09-24 09:46:44 -0500

    Check if user is online

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   before_action :authenticate_user!
   before_action :set_global_variables, if: :user_signed_in?
+  after_action :user_activity
 
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
@@ -13,5 +14,9 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
+  end
+
+  def user_activity
+    current_user.try :touch
   end
 end
